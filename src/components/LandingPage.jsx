fcc5f9f0c1b7d662ccb85c09e75cbabd80fe7e2a
Kean Bayneslow <kean.bayneslow@gmail.com>
2023-10-14 09:29:33 +1100

    adds unstyled landing page

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactPlayer from 'react-player';
+import '../styles/landingPage.css';
+
+
+function LandingPage() {
+  return (
+    <div className="landing-page">
+      <ReactPlayer
+        className="video-background"
+        url="/assets/videos/beer.mp4"
+        playing
+        loop
+        muted
+      />
+      <div className="content">
+        <h1>Welcome To Your Pub Crawl Planner</h1>
+        <p>Explore the wide world of breweries, create your dream pub crawl and add your own hidden gems.</p>
+      </div>
+    </div>
+  );
+}
+
+export default LandingPage;

13535a04a2c5b2b6ef2c070de695d3ea12d4f0e3
Kean Bayneslow <kean.bayneslow@gmail.com>
2023-10-14 09:57:11 +1100

    styles landing page

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactPlayer from 'react-player';
 import '../styles/landingPage.css';
 
-
 function LandingPage() {
   return (
     <div className="landing-page">
@@ -13,9 +12,11 @@ function LandingPage() {
         loop
         muted
       />
-      <div className="content">
-        <h1>Welcome To Your Pub Crawl Planner</h1>
-        <p>Explore the wide world of breweries, create your dream pub crawl and add your own hidden gems.</p>
+      <div className="video-wrapper">
+        <div className="content">
+          <h1>Welcome To Your Pub Crawl Planner</h1>
+          <p>Explore the wide world of breweries, create your dream pub crawl, and add your own hidden gems.</p>
+        </div>
       </div>
     </div>
   );
